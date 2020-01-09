4ba5ecb6b1c6b93cb09c6c4b2c50ebb05f19a38d
Will Russell <will@will-russell.com>
2020-01-09 19:08:15 +0000

    Added internet access

--- app/src/main/java/com/willrussell/hackathon_timer_android_client/MainActivity.java
+++ app/src/main/java/com/willrussell/hackathon_timer_android_client/MainActivity.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
@@ -21,18 +25,18 @@ public class MainActivity extends AppCompatActivity {
     private MaterialButton connectionButton;
     private Handler uiHandler;
 
-
     final int PORT = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         time = findViewById(R.id.time);
         connectionButton = findViewById(R.id.connection_button);
         socket = new Socket();
-
         uiHandler = new Handler();
     }
 
@@ -59,13 +63,13 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-
-
     }
 
     private void connect(String ip) throws IOException {
-        socket.connect(new InetSocketAddress(ip, PORT), 2000);
-        socket.close();
+        InetAddress serverAddr = InetAddress.getByName("Wills-MacBook-Pro.local");
+        socket.connect(new InetSocketAddress(serverAddr, PORT));
+        NetworkThread networkThread = new NetworkThread(socket);
+        new Thread(networkThread).start();
     }
 
     private void disconnect() throws IOException {
@@ -81,9 +85,19 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
+            while(!Thread.currentThread().isInterrupted()) {
+                try {
+                    InputStream in = socket.getInputStream();
+                    DataInputStream dataIn = new DataInputStream(in);
+                    String output = dataIn.readUTF();
+                    System.out.println("OUTPUT: " + output);
+                    uiHandler.post(new UIThread(1));
+                } catch (IOException e) {
+                    break;
+                }
+            }
             // Check for new data
             // Only allow for UIThread to be updated if time has finished
-            uiHandler.post(new UIThread(1));
             // send data back that the timer has started and how long is left.
         }
     }
@@ -100,6 +114,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             // Update time
+            System.out.println("TIME: " + minutes);
             if (startTimer(minutes)) {
                 this.timer = true;
             }
