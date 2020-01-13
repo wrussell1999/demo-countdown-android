fd4ba22068e923e1b2c52129235f68e71b9b736b
Will Russell <will@will-russell.com>
2020-01-13 16:36:36 +0000

    Added menu for about page

--- app/src/main/java/com/willrussell/hackathon_demo_timer_android/MainActivity.java
+++ app/src/main/java/com/willrussell/hackathon_demo_timer_android/MainActivity.java
@@ -2,7 +2,10 @@ package com.willrussell.hackathon_demo_timer_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
