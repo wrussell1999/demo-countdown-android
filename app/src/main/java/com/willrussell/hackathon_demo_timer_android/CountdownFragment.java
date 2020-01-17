21aaa02d1402f165e6182a265674db6174e25ad6
Will Russell <will@will-russell.com>
2020-01-17 01:47:44 +0000

    Moved countdown classes into own package

--- app/src/main/java/com/willrussell/hackathon_demo_timer_android/CountdownFragment.java
+++ app/src/main/java/com/willrussell/hackathon_demo_timer_android/CountdownFragment.java
@@ -14,20 +14,16 @@ import com.android.volley.RequestQueue;
 
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.willrussell.hackathon_demo_timer_android.countdown_threads.CountdownThread;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class CountdownFragment extends Fragment {
 
-    private final String SERVER_URL = "https://2c0e0446.ngrok.io"; // Must be https
+    private final String SERVER_URL = "https://4b2f272e.ngrok.io/get_countdown"; // Must be https
     private TextView countdownView;
     private View background;
-    private Handler handler;
 
     public static CountdownFragment newInstance() {
         CountdownFragment fragment = new CountdownFragment();
@@ -39,7 +35,6 @@ public class CountdownFragment extends Fragment {
         View view = inflater.inflate(R.layout.countdown_fragment, container, false);
         countdownView = view.findViewById(R.id.time);
         background = view;
-        handler = new Handler();
 
         RequestQueue queue = Volley.newRequestQueue(view.getContext());
 
@@ -47,7 +42,7 @@ public class CountdownFragment extends Fragment {
                 response -> {
                     try {
                         JSONObject obj = new JSONObject(response);
-                        CountdownThread countdownThread = new CountdownThread(obj.getString("time"));
+                        CountdownThread countdownThread = new CountdownThread(obj.getString("time"), this.countdownView);
                         new Thread(countdownThread).start();
                     } catch (JSONException e) {
                         e.printStackTrace();
@@ -59,81 +54,4 @@ public class CountdownFragment extends Fragment {
         return view;
     }
 
-    class CountdownThread implements Runnable {
-        private String endTime;
-        private String countdown;
-        private boolean flash;
-        private boolean finish;
-
-        public CountdownThread(String endTime) {
-            this.endTime = endTime;
-            this.countdown = "";
-            this.flash = false;
-            this.finish = false;
-        }
-
-        @Override
-        public void run() {
-            while (!Thread.currentThread().isInterrupted()) {
-                try {
-                    countdown = getTime();
-                    handler.post(new UIThread(this.countdown, this.flash));
-                    if (finish) {
-                        break;
-                    }
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        public String getTime() throws ParseException {
-            Date dateNow = new Date();
-            SimpleDateFormat formatterEnd = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss");
-            Date dateEnd = formatterEnd.parse(endTime);
-
-            long timeLeft = (dateEnd.getTime() - dateNow.getTime()) / 1000;
-            String timeFormatted;
-            long minutes = timeLeft / 60;
-            long seconds = timeLeft % 60;
-
-            if (minutes == 0 && seconds <= 10 && seconds % 2 == 0) {
-                this.flash = true;
-            } else {
-                this.flash = false;
-            }
-
-            if (minutes == 0 && seconds < 0 && seconds > -10) {
-                timeFormatted = "0:00";
-            } else if (minutes == 0 && seconds <= -10) {
-                this.finish = true;
-                this.flash = false;
-                timeFormatted = "0:00";
-            } else {
-                timeFormatted = String.format("%d:%02d", (int) minutes, (int) seconds);
-            }
-
-            return timeFormatted;
-        }
-    }
-
-    class UIThread implements Runnable {
-        private String output;
-        private boolean flash;
-
-        public UIThread(String output, Boolean flash) {
-            this.output = output;
-            this.flash = flash;
-        }
-
-        @Override
-        public void run() {
-            countdownView.setText(output);
-            if (flash) {
-                //background.setBackgroundColor(getResources().getColor(android.R.color.holo_red_dark));
-            } else {
-                //background.setBackgroundColor(getResources().getColor(android.R.color.background_light));
-            }
-        }
-    }
 }
