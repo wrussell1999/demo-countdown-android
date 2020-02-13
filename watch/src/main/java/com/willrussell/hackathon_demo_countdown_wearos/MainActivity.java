98819a116af2184b2ad566c3f9a2377d11053756
Will Russell <will@will-russell.com>
2020-02-13 01:50:47 +0000

    Add current time to display

--- watch/src/main/java/com/willrussell/hackathon_demo_countdown_wearos/MainActivity.java
+++ watch/src/main/java/com/willrussell/hackathon_demo_countdown_wearos/MainActivity.java
@@ -4,18 +4,48 @@ import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private TextView countdown;
+    private TextView realTime;
+    private TimeThread thread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        countdown = findViewById(R.id.time);
+        realTime = findViewById(R.id.actual_time);
 
-        // Enables Always-on
+        thread = new TimeThread();
+        new Thread(thread).start();
+
         setAmbientEnabled();
+    }
+
+    class TimeThread implements Runnable {
+        private String time;
+
+
+        @Override
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                time = getTime();
+                runOnUiThread(() -> {
+                    realTime.setText(time);
+                });
+            }
+        }
+
+        public String getTime() {
+            SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+            Date date = new Date();
+            String time = formatter.format(date);
+            return time;
+        }
     }
 }
